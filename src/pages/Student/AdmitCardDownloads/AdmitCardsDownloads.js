276719048d66cd0de2dc31a8637cf71b028d4a5c
Shamsalfahad <shamsalfahad3@gmail.com>
2023-01-14 22:25:07 +0600

    admit card download

---
+++ src/pages/Student/AdmitCardDownloads/AdmitCardsDownloads.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./AdmitCardsDownloads.css"
+
+const AdmitCardsDownloads = () => {
+    return (
+        <div className='bg-light pb-3 background'>
+    <div>
+        <h4 className=''><i class="bi bi-text-indent-left"></i>Online Admit Card Download / Print</h4>
+    </div>
+    <hr></hr>
+            <div className="wrapper ">
+        <div className="grid">
+          <div className="item bold">
+Select Semester</div>
+          <div className="item"><select id="country" name="country">
+      <option value="australia">Fall   2022 (4M)</option>
+      <option value="canada">Summer 2022 (4M)</option>
+      <option value="usa">Spring 2022 (4M)</option>
+      <option value="usa">Fall   2021 (4M)</option>
+      <option value="usa">Summer 2021 (4M)</option>
+      <option value="usa">Spring 2021 (4M)</option>
+      <option value="usa">Fall   2020 (4M)</option>
+      <option value="usa">Summer 2020 (4M)</option>
+      <option value="usa">Spring 2020 (4M)</option>
+      <option value="usa">Fall   2019 (4M)</option>
+      <option value="usa">Summer 2019 (4M)</option>
+      <option value="usa">Spring 2019 (4M)</option>
+    </select></div>
+          <div className="item bold">
+          Midterm Admit Card</div>
+          <div className="item"><button type="button" className="btn btn-success btn-sm">Download / Print</button></div>
+          <div className="item bold">Final Term Admit Card</div>
+          <div className="item"><p className='text-danger'>Final Term Payment Incomplete, Please Clear Payments for get Admit Card!</p> </div>
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default AdmitCardsDownloads;
--- src/pages/Student/Index.js
+++ src/pages/Student/Index.js
@@ -13,7 +13,7 @@ import Minibar from "../../Components/MiniBar/Minibar";
 import Dashboard from "./Dashboard/Dashboard";
 import BottomNavBar from "../../Components/BottomNavBar/BottomNavBar";
 import ChangePassword from "./ChangePassword/ChangePassword";
-
+import AdmitCardsDownloads from "./AdmitCardDownloads/AdmitCardsDownloads";
 // lazy loading components go here
 const Profile = React.lazy(() => import("./Profile/Profile"));
 
@@ -95,6 +95,41 @@ const Index = () => {
                   </Suspense>
                 </ErrorBoundary>
               }
+              
+            />
+              <Route
+              path="/admit_card_download"
+              element={
+                <ErrorBoundary fallback={<PageLoadingFail />}>
+                  <Suspense
+                    fallback={
+                      <>
+                        <SkeletonTheme
+                          baseColor="#9298a8"
+                          highlightColor="#444"
+                        >
+                          <div className="row">
+                            <div className="col-4">
+                              <Skeleton count={5} />
+                            </div>
+                            <div className="col-8">
+                              <Skeleton count={5} />
+                            </div>
+                          </div>
+
+                          <div>
+                            <Skeleton count={50} />
+                          </div>
+                        </SkeletonTheme>
+                      </>
+                    }
+                  >
+                    <MetaTag MetaTagTitle="admit_card_download" />
+                    <AdmitCardsDownloads></AdmitCardsDownloads>
+                  </Suspense>
+                </ErrorBoundary>
+              }
+              
             />
             
             <Route path="/*" element={<Error404 />} />

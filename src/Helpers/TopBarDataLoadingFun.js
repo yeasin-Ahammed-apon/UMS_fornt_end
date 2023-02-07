ae4ce3ee1feac530499958b4b48a5f7d87ed249a
yeasin-Ahammed-apon <yeasinahammedapon@gmail.com>
2023-02-07 23:53:19 +0600

    ya me ha me ha

--- src/Helpers/TopBarDataLoadingFun.js
+++ src/Helpers/TopBarDataLoadingFun.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { Topbarloaderdata } from "../App";
 
 export const HelperForSetTopbardata = (value) => {
+  console.log("Ola Amigos");
   const setTopbardata = useContext(Topbarloaderdata);  
-  setTopbardata(value);   
+  setTopbardata(value);  
+   
   
 };
